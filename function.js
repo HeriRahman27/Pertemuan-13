5c62ad1b3d227734aee64965319d70f88d064578
HeriRahman27 <87768814+HeriRahman27@users.noreply.github.com>
2021-07-21 23:15:01 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+function dblKlik(){
+    var myImage = new Image(400, 250);
+	myImage.src = 'https://i.ytimg.com/vi/5yqV0P2iXUM/mqdefault.jpg';
+	x = document.getElementById("lol");
+	x.appendChild(myImage);	
+}
